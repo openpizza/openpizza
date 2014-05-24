01a5d183d37d0a8bf2e603f2b31ae61dfd4aafa3
flower-pot <flower-pot@flowerpot.io>
2014-05-24 18:04:15 +0000

    add redirect to login when trying to access admin interface

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,7 +4,7 @@ RailsAdmin.config do |config|
 
   ## == Devise ==
   config.authenticate_with do
-    redirect_to main_app.root_path unless warden.user && warden.user.admin?
+    redirect_to new_user_session_path unless warden.user && warden.user.admin?
   end
   config.current_user_method(&:current_user)
 

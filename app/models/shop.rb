0308fa99cabaceaa8b35b80f87711ee1227f8a4b
flower-pot <flower-pot@flowerpot.io>
2014-05-24 17:56:26 +0000

    add products method to shops

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,8 @@
 class Shop < ActiveRecord::Base
   belongs_to :user
   belongs_to :address
+  has_many :product_categories
+  has_many :products, through: :product_categories
   validates :address, :user, presence: true
 
   def rating

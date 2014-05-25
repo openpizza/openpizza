0c4c13350a7464b2a3caca98ae4ff94e13716990
Patrick Gebert <patrick.gebert@web.de>
2014-05-25 12:27:23 +0200

    Added Product selection

--- app/assets/javascripts/controllers/orders.js
+++ app/assets/javascripts/controllers/orders.js
@@ -6,18 +6,38 @@
 angular.module('pizza').controller('OrderController', ['$scope', '$location', '$routeParams', '$resource', 'Shops', 'Products', 'Global', function ($scope, $location, $routeParams, $resource, Shops, Products, Global) {
 
     $scope.shop = {};
-
-    $scope.products = [{
-        category: "Pizza",
-        name: 'Pizza Salami'
-    }];
-    $scope.shop = {};
+    $scope.currentOrder = {
+        nickname: '',
+        products: [],
+        comment: '',
+        price: 0
+    };
 
     $scope.getShop = function(shopId) {
-
         Shops.single.get({shopId: shopId}, function(res) {
             $scope.shop = res;
         });
+    };
+
+    $scope.add = function(product) {
+
+        var found = false;
+
+        for(var i = 0; i < $scope.currentOrder.products.length; i++) {
+            if ($scope.currentOrder.products[i].id == product.id) {
+                $scope.currentOrder.products[i].quantity ++;
+                found = true;
+            }
+        }
+
+        if (!found) {
+            $scope.currentOrder.products.push({
+                id: product.id,
+                name: product.name,
+                quantity: 1
+            });
+        }
+        $scope.currentOrder.price += product.price;
     };
 
 //    $scope.getProducts = function(shopId) {

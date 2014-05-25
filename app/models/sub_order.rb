862057f4d1bfa3f4344b053cfee3e1d369c3c3a4
Christoph Heer <Christoph.Heer@gmail.com>
2014-05-25 09:48:21 +0200

    Update controler for sub_order for nested api

--- app/models/sub_order.rb
+++ app/models/sub_order.rb
@@ -2,4 +2,21 @@ class SubOrder < ActiveRecord::Base
   belongs_to :order
 
   validates :order, :nickname, presence: true
+
+  def self.of_order(order_uuid)
+    suborders = SubOrder.joins(:order).where(orders: { uuid: order_uuid })
+    if suborders.nil?
+      raise ActiveRecord::RecordNotFound
+    end
+    suborders
+  end
+
+  def self.find_by_order_and_nickname(order_uuid, nickname)
+    suborders = SubOrder.joins(:order).where(orders: { uuid: order_uuid }, nickname: nickname).first
+    if suborders.nil?
+      raise ActiveRecord::RecordNotFound
+    end
+    suborders
+  end
+
 end

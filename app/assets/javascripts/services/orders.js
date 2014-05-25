8e05b4a1a192e735efb1134bc89009ffe36dd5dc
Patrick Gebert <patrick.gebert@web.de>
2014-05-25 11:43:02 +0200

    Moved product table to order page

--- app/assets/javascripts/services/orders.js
+++ app/assets/javascripts/services/orders.js
@@ -1,3 +1,10 @@
 /**
  * Created by Patrick on 25.05.2014.
  */
+
+angular.module('pizza').factory('Shops', ['$resource', function($resource) {
+    return {
+        all: $resource('http://openpizza.apiary-mock.com/shops?:postcode'),
+        single: $resource('http://openpizza.apiary-mock.com/shops/:shopId')
+    };
+}]);

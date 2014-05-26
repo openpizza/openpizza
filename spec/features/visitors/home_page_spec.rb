ac0eb1ed88eb16650db51346346212fad0f5678a
flower-pot <flower-pot@flowerpot.io>
2014-05-26 09:12:56 +0000

    replace all open-pizza with OpenPizza

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -12,7 +12,7 @@ feature 'Home page' do
   #   Then I see "Welcome"
   scenario 'visit the home page' do
     visit root_path
-    expect(page).to have_content 'Open-Pizza'
+    expect(page).to have_content 'OpenPizza'
   end
 
 end

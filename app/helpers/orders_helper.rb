4d8a223436dd5ff079d4ffc4f7827bbd8ac03522
flower-pot <flower-pot@flowerpot.io>
2014-05-26 10:43:41 +0000

    refactor share urls to helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,9 @@
 module OrdersHelper
+  def share_url(order)
+    root_url[0..-2] + new_order_item_path(order.uuid)
+  end
+
+  def qrcode_url(order)
+    "openpizza" + root_url[4..-2] + order_path(order.uuid)
+  end
 end
